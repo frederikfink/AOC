14974dd9865b53ebb7a29f56673de5ec9206e30c
Frederik Fink Højriis <frfh@itu.dk>
2022-12-07 08:23:21 +0100

    update

--- Day2/test.py
+++
@@ -1,3 +0,0 @@
-print(
-    sum([x[1]-87+(3 if x[0] == -23+x[1]else 6 if (x[0]-64) % 3 == -88+x[1]else 0)
-      for x in map(lambda l:list(map(ord, l.strip().split(" "))), open("input.txt").readlines())]))
